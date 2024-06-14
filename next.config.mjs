4172c51cd58ca17e3029f99c7fa05ab2e441c4af
OriginalKazdov <juandovzak@gmail.com>
2024-06-14 20:02:04 -0300

    prueba de oro cruza los dedos pibe

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env: {
+      DATABASE_URL: process.env.DATABASE_URL,
+      PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID,
+      PAYPAL_CLIENT_SECRET: process.env.PAYPAL_CLIENT_SECRET,
+      PAYPAL_ENVIRONMENT: process.env.PAYPAL_ENVIRONMENT,
+      NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+      NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+      NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    },
+  };
+  
+  export default nextConfig;
+  
